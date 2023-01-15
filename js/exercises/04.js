cdd46087d40082eef3cca5c6f12d3e04573e0776
ansachuk <ansachuk@icloud.com>
2023-01-16 00:03:17 +0200

    off

---
+++ js/exercises/04.js
@@ -0,0 +1,13 @@
+//1.vars
+let number = 30;
+let minNumber = number - 1;
+
+for (let i = minNumber; i > 1; i = i - 1) {
+  if (number % i === 0) {
+    console.log(i);
+  } else {
+    console.log("nems");
+  }
+}
+//2.Знайти прості дільники
+//3.
