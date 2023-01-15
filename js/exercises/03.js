ad3f21444f2b55939db1c2587f5437dad7bb8c76
ansachuk <ansachuk@icloud.com>
2023-01-15 23:13:33 +0200

    add ex#3

---
+++ js/exercises/03.js
@@ -0,0 +1,18 @@
+let c = 0;
+let a = 0;
+let b = 1;
+let sum = 0;
+const number = 4000000;
+
+do {
+  //   console.log(c);
+  c = a + b;
+  a = b;
+  b = c;
+
+  if (c % 2 === 0) {
+    sum += c;
+  }
+} while (a <= number);
+
+console.log(sum);
