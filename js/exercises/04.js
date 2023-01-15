8a9142c7237496e7c7d6df3cfbf3c740bf1168d5
ansachuk <ansachuk@icloud.com>
2023-01-16 01:34:03 +0200

    test 3 done!

--- js/exercises/04.js
+++ js/exercises/04.js
@@ -1,16 +1,20 @@
-//1.vars
 let number = 600851475143;
 
-for (let i = number - 1; i > 1; i -= 1) {
+for (let i = 2; i < number; i += 1) {
   if (number % i === 0) {
-    number = i;
-    for (let j = number; j > 1; j -= 1) {
-      if (number % j !== 0) {
+    number /= i;
+    console.log(number);
+    for (let i = 2; i < number; i += 1) {
+      if (number % i === 0) {
+        number /= i;
         console.log(number);
-        break;
+        for (let i = 2; i < number; i += 1) {
+          if (number % i === 0) {
+            number /= i;
+            console.log(number);
+          }
+        }
       }
     }
   }
 }
-//2.Знайти прості дільники
-//3.
