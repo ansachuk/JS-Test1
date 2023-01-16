5fd1fdbb08ecf0d3f1b7df89f54effd49e279ccb
ansachuk <ansachuk@icloud.com>
2023-01-16 21:19:26 +0200

    trying another features

--- js/exercises/05.js
+++ js/exercises/05.js
@@ -1,15 +1,12 @@
-const obA = {
-  name: "an",
-  age: 20,
+const name = "an";
+const age = 21;
+
+const userProfile = {
+  name,
+  age,
+  funk() {
+    console.log("fuck you");
+  },
 };
-const copyA = obA;
 
-console.log(copyA.age);
-
-copyA.age = 21;
-
-console.log(obA.age);
-
-copyA.sex = "male";
-
-console.log(obA);
+console.log(userProfile.funk());
