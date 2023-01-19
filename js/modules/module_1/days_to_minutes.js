00cb2b6f3e210bb7360cea69b2198ae3e3ebe81a
ansachuk <ansachuk@icloud.com>
2023-01-19 22:29:25 +0200

    modernize 'day to minutes' and 'minutes to days'

--- js/modules/module_1/days_to_minutes.js
+++ js/modules/module_1/days_to_minutes.js
@@ -1,9 +1,17 @@
+// const daysToMinutes = days * 1440;
+// const hoursToMinutes = hours * 60;
+
+// const totalMinutes = daysToMinutes + hoursToMinutes + minutes;
+// console.log(`You have ${totalMinutes} minutes!`);
+
+const transformDaysToMinute = function (days, hours, minutes) {
+  days *= 1440;
+  hours *= 60;
+  return `You have ${days + hours + minutes} minutes!`;
+};
+
 const days = Number(prompt("How many days?"));
 const hours = Number(prompt("How many hours?"));
 const minutes = Number(prompt("How many minutes?"));
 
-const daysToMinutes = days * 1440;
-const hoursToMinutes = hours * 60;
-
-const totalMinutes = daysToMinutes + hoursToMinutes + minutes;
-console.log(`You have ${totalMinutes} minutes!`);
+console.log(transformDaysToMinute(days, hours, minutes));
--- js/modules/module_1/time_to_day.js
+++ js/modules/module_1/time_to_day.js
@@ -1,7 +1,14 @@
-const userMinutes = Number(prompt("How many minutes you have?"));
-const minutes = userMinutes % 60;
-const hours = Math.floor(userMinutes / 60);
-const hoursFloor = hours % 24;
-const days = Math.floor(hours / 24);
+// const userMinutes = Number(prompt("How many minutes you have?"));
 
-console.log(` You have ${days} days, ${hoursFloor} hours, ${minutes} minutes.`);
+const transformMinutesToDays = function (userMinutes) {
+  const minutes = userMinutes % 60;
+  const fullHours = Math.floor(userMinutes / 60);
+  const hoursFloor = fullHours % 24;
+  const fullDays = Math.floor(fullHours / 24);
+
+  return ` You have ${fullDays} days, ${hoursFloor} hours, ${minutes} minutes.`;
+};
+
+const enteredMinutes = Number(prompt("How many minutes you have?"));
+
+console.log(transformMinutesToDays(enteredMinutes));
