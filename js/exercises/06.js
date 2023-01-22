85cefdd37c5d189910fa50a1cf9118f0263ead03
ansachuk <ansachuk@icloud.com>
2023-01-22 12:02:10 +0200

    add calculator

---
+++ js/exercises/06.js
@@ -0,0 +1,32 @@
+// Напишіть через світч калькулятор
+// Потрібно ввести перше число в prompt, потім математичну операцію
+// Тоді ввести наступне число
+
+// const firstNumber = prompt("Enter first number:");
+// const secondNumber = prompt("Enter second number:");
+// const operation = prompt("Enter +,-,*,/");
+
+// let result = 0;
+
+// switch (operation) {
+//     case "+":
+//     result = firstNumber + secondNumber;
+//     break;
+
+//   case "-":
+//     result = firstNumber - secondNumber;
+//     break;
+
+//   case "*":
+//     result = firstNumber * secondNumber;
+//     break;
+
+//   case "/":
+//     result = firstNumber / secondNumber;
+//     break;
+
+//   default:
+//     console.log("Error");
+// }
+
+// console.log(result);
