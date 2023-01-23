02a60709c72eb545b0c3ba9dc78dcfdae210a638
ansachuk <ansachuk@icloud.com>
2023-01-23 22:36:17 +0200

    add ex#5

--- js/exercises/05.js
+++ js/exercises/05.js
@@ -1,5 +1,46 @@
-function sayMyName() {
-  console.log("Haisenberg");
-}
+const isDevideOnAllFromAToB = function (a, b) {
+  for (let i = 1; i < 10000000000; i += 1) {
+    let ok = true;
+    for (let j = a; j <= b; j += 1) {
+      if (i % j > 0) {
+        ok = false;
+        break;
+      }
+    }
+    if (!ok) continue;
+    return i;
+    break;
+  }
 
-setTimeout(sayMyName, 3000);
+  // for (let i = a; i <= b; i += 1) {
+  //   for (let j = 2; j <= 1000000000; j += 1) {
+  //     if (j % i > 0) {
+  //       ok = false;
+  //       break;
+  //     }
+  //   }
+  //   if (!ok) {
+  //     continue;
+  //   }
+
+  //   return j;
+  // }
+};
+
+console.log(isDevideOnAllFromAToB(1, 20));
+
+//1. Make vars
+
+//2. Make loop
+
+//3. Make loop from 2 to 20
+
+//4.Make if devides
+
+//5.Return number
+
+// function sayMyName() {
+//   console.log("Haisenberg");
+// }
+
+// setTimeout(sayMyName, 3000);
--- js/exercises/06.js
+++ js/exercises/06.js
@@ -2,32 +2,32 @@
 // Потрібно ввести перше число в prompt, потім математичну операцію
 // Тоді ввести наступне число
 
-// const firstNumber = prompt("Enter first number:");
-// const secondNumber = prompt("Enter second number:");
-// const operation = prompt("Enter +,-,*,/");
+const firstNumber = prompt("Enter first number:");
+const secondNumber = prompt("Enter second number:");
+const operation = prompt("Enter +,-,*,/");
 
-// let result = 0;
+let result = 0;
 
-// switch (operation) {
-//     case "+":
-//     result = firstNumber + secondNumber;
-//     break;
+switch (operation) {
+  case "+":
+    result = firstNumber + secondNumber;
+    break;
 
-//   case "-":
-//     result = firstNumber - secondNumber;
-//     break;
+  case "-":
+    result = firstNumber - secondNumber;
+    break;
 
-//   case "*":
-//     result = firstNumber * secondNumber;
-//     break;
+  case "*":
+    result = firstNumber * secondNumber;
+    break;
 
-//   case "/":
-//     result = firstNumber / secondNumber;
-//     break;
+  case "/":
+    result = firstNumber / secondNumber;
+    break;
 
-//   default:
-//     console.log("Error");
-// }
+  default:
+    console.log("Error");
+}
 
 // console.log(result);
 
