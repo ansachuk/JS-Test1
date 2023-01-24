089b7d65ae62b5d2534d3606a11c14ab0ed512f0
ansachuk <ansachuk@icloud.com>
2023-01-24 23:52:59 +0200

    add obj base

---
+++ js/modules/objects-base.js
@@ -0,0 +1,27 @@
+const propertyName = "Drill";
+const propertyRating = "rating";
+const trackCount = 5;
+
+const playlist = {
+  name: propertyName,
+  rating: 4.6,
+  tracks: ["track 1", "track 2", "track 3", "track 4", "track 5"],
+  trackCount,
+};
+
+// console.log(playlist);
+// console.log(playlist.name);
+// // Diff ways to clg same keys
+// console.log(playlist.rating);
+// console.log(playlist[propertyRating]);
+// //
+// console.table(playlist.tracks);
+// console.log(playlist.trackCount);
+
+const book = {
+  name: "The Dark Tower: Gunslinger",
+  author: "Steven King",
+  genres: ["horor", "fantasy", "western"],
+  rating: 8.9,
+  pagesCount: 409,
+};
