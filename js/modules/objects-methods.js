69e8df37c07bd23e9e868c502cb859c6071b33e7
ansachuk <ansachuk@icloud.com>
2023-01-27 23:09:51 +0200

    interesting ex

--- js/modules/objects-methods.js
+++ js/modules/objects-methods.js
@@ -1,19 +1,25 @@
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+const {
+  today: {
+    high: highToday,
+    low: lowToday,
+    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
 
-function calculateTotalPrice(productName) {
-  // Change code below this line
-  for (let product of products) {
-    if (productName === product.name) {
-      return product.price * product.quantity;
-    }
-  }
-
-  // Change code above this line
-}
-
-console.log(calculateTotalPrice("a"));
+  tomorrow: {
+    high: highTomorrow,
+    low: lowTomorrow,
+    icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+} = forecast;
