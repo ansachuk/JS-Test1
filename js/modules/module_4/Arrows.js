0caae5db1723e39621e4950d153d3126ab48846b
ansachuk <ansachuk@icloud.com>
2023-02-04 18:30:11 +0200

    finish tue module 4

---
+++ js/modules/module_4/Arrows.js
@@ -0,0 +1,11 @@
+function fnA(a) {
+  return a * 2;
+}
+
+const fnB = function (b) {
+  return b * 3;
+};
+
+const fnC = (c) => c * 3;
+
+const fnArrey = (d) => ({ a: 5 });
--- js/modules/module_4/forEach.js
+++ js/modules/module_4/forEach.js
@@ -19,3 +19,8 @@ numbers.forEach((element, index) => {
 });
 
 // console.log(Math.max(...numbers));
+// const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, "shkgkw"];
+
+// array1.forEach((element, index) => {
+//   console.log(`${index}:${element}`);
+// });
