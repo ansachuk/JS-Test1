2a0f4ce70c4a205a0a309cb0a4bcb036785f8b65
ansachuk <ansachuk@icloud.com>
2023-02-05 23:07:45 +0200

    add every,some,find,findIndex,reduce

---
+++ js/modules/module_4/Finds.js
@@ -0,0 +1,30 @@
+//Finds() like filter(), but searching only first match
+
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+
+const findMatch = colorPickerOptions.find(
+  (option) => option.color === "#3F51B5"
+);
+
+// console.log(findMatch);
+
+// console.log(colorPickerOptions.find((option) => option.label === "blue"));
+// console.log(colorPickerOptions.find((option) => option.label === "pink"));
+// console.log(colorPickerOptions.find((option) => option.label === "white"));
+
+// Findindex()
+
+console.log(colorPickerOptions.findIndex((option) => option.label === "red"));
+console.log(
+  colorPickerOptions.findIndex((option) => option.color === "#E91E63")
+);
+console.log(colorPickerOptions.findIndex((option) => option.label === "white"));
+console.log(
+  colorPickerOptions.findIndex((option) => option.label === "indigo")
+);
---
+++ js/modules/module_4/every-some.js
@@ -0,0 +1,15 @@
+// every() return true if every elelment match test of callback
+
+const fruits = [
+  { name: "apple", quantity: 3 },
+  { name: "pear", quantity: 4 },
+  { name: "peach", quantity: 1 },
+  { name: "coconut", quantity: 0 },
+  { name: "watermelon", quantity: 1 },
+  { name: "orange", quantity: 2 },
+];
+
+console.log(fruits.every((fruit) => fruit.quantity > 0));
+
+// some() return true if even one element match test callback
+console.log(fruits.some((fruit) => fruit.quantity === 0));
