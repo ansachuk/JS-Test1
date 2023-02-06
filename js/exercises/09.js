f1ac06e51c1c01e1eea7399f82eb295336e36230
ansachuk <ansachuk@icloud.com>
2023-02-06 23:01:29 +0200

    add sort by balance ex

--- js/exercises/09.js
+++ js/exercises/09.js
@@ -1,41 +1,70 @@
-const books = [
+const arr = [
   {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
   },
   {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
   },
   {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
   },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+  },
 ];
-// Change code below this line
 
-const sortedByAuthorName = [...books].sort((firstb, secondb) =>
-  firstb.author.localeCompare(secondb.author)
-);
+const sortByAscendingBalance = (users) =>
+  [...users].sort((a, b) => a.balance - b.balance);
 
-const sortedByReversedAuthorName = [...books].sort((firstb, secondb) =>
-  secondb.author.localeCompare(firstb.author)
-);
-
-const sortedByAscendingRating = [...books].sort(
-  (firstb, secondb) => firstb.rating - secondb.rating
-);
-
-const sortedByDescentingRating = [...books].sort(
-  (firstb, secondb) => secondb.rating - firstb.rating
-);
-
-console.log(sortedByAuthorName);
-console.log(sortedByReversedAuthorName);
-console.log(sortedByAscendingRating);
-console.log(sortedByDescentingRating);
+console.log(sortByAscendingBalance(arr));
