a33ed3cff33e5137ade1d18f172047642607e598
ansachuk <ansachuk@icloud.com>
2023-02-06 23:32:52 +0200

    chaining ex

--- js/exercises/09.js
+++ js/exercises/09.js
@@ -1,70 +1,33 @@
-const arr = [
+const books = [
   {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male",
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
   },
   {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female",
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
   },
   {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male",
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
   },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
   {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female",
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    gender: "male",
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    gender: "male",
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female",
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
   },
 ];
+const MIN_BOOK_RATING = 8;
+// Change code below this line
 
-const sortByAscendingBalance = (users) =>
-  [...users].sort((a, b) => a.balance - b.balance);
-
-console.log(sortByAscendingBalance(arr));
+const names = [...books]
+  .filter(({ rating }) => rating > MIN_BOOK_RATING)
+  .map(({ author }) => author)
+  .sort((a, b) => a.localeCompare(b));
+//Доповни код таким чином, щоб у змінній names вийшов масив імен авторів в
+//алфавітному порядку, рейтинг книг яких більший за значення змінної MIN_BOOK_RATING.
+console.log(names);
