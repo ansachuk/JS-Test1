146dd9dbae442a1a8a668e7dc17d4b4edeea0a41
ansachuk <ansachuk@icloud.com>
2023-02-07 11:02:01 +0200

    add ex#9 , this and add type module to all script

--- js/exercises/09.js
+++ js/exercises/09.js
@@ -1,33 +1,79 @@
-const books = [
+const arr = [
   {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
   },
   {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
   },
   {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
   },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
   {
-    title: "The Dreams in the Witch House",
-    author: "Howard Lovecraft",
-    rating: 8.67,
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: [
+      "Jacklyn Lucas",
+      "Linda Chapman",
+      "Adrian Cross",
+      "Solomon Fokes",
+    ],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
   },
 ];
-const MIN_BOOK_RATING = 8;
-// Change code below this line
 
-const names = [...books]
-  .filter(({ rating }) => rating > MIN_BOOK_RATING)
-  .map(({ author }) => author)
-  .sort((a, b) => a.localeCompare(b));
-//Доповни код таким чином, щоб у змінній names вийшов масив імен авторів в
-//алфавітному порядку, рейтинг книг яких більший за значення змінної MIN_BOOK_RATING.
-console.log(names);
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter((user) => user.gender === gender)
+    .reduce((total, { balance }) => (total += balance), 0);
+
+console.log(getTotalBalanceByGender(arr, "female"));
+
+//Доповни функцію getTotalBalanceByGender(users, gender) таким чином, щоб вона повертала загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
