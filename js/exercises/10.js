47f2e4417b0aa1e6070be9307c2742a81992c12c
ansachuk <ansachuk@icloud.com>
2023-02-21 00:53:51 +0200

    hw 5 ex 10

--- js/exercises/10.js
+++ js/exercises/10.js
@@ -1,18 +1,49 @@
-const ancestor = {
-  name: "Paul",
-  age: 83,
-  surname: "Dawson",
-  heritage: "Irish",
-};
-// Change code below this line
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
 
-const parent = Object.create(ancestor);
-parent.name = "Stacey";
-parent.surname = "Moore";
-parent.age = 54;
+  getItems() {
+    return this.items;
+  }
 
-const child = Object.create(parent);
-child.name = "Jason";
-child.age = 27;
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.items.splice(
+      this.items.findIndex((item) => item === itemToRemove),
+      1
+    );
+  }
+}
 
 // Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+/* 
+*
+Завдання 10/20
+
+Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+
+Оголоси наступні методи класу:
+
+getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
+
+addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
+
+removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.
+
+
+Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+*/
+
+//    hw 5 ex 5
