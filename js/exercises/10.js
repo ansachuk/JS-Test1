899656a906f0e5b4c32efc697a489a2a8c75f2f1
ansachuk <ansachuk@icloud.com>
2023-02-21 14:27:47 +0200

    hw 5 ex 20

--- js/exercises/10.js
+++ js/exercises/10.js
@@ -1,35 +1,64 @@
-class Car {
-  static #MAX_PRICE = 50000;
-  // Change code below this line
+"use strict";
 
-  static checkPrice(price) {
-    return price > Car.#MAX_PRICE
-      ? "Error! Price exceeds the maximum"
-      : "Success! Price is within acceptable limits";
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
   }
 
-  // Change code above this line
-  constructor({ price }) {
-    this.price = price;
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
   }
 }
 
-const audi = new Car({ price: 36000 });
-const bmw = new Car({ price: 64000 });
+class Admin extends User {
+  // Change code below this line
 
-console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
-console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
 
-/**Завдання 17/20
+  constructor({ email, accessLevel, blacklistedEmails = [] }) {
+    super(email);
+    this.accessLevel = accessLevel;
+    this.blacklistedEmails = blacklistedEmails;
+  }
 
-Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
 
-Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
-В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
-Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
 
-ТЕСТИ
+  // Change code above this line
+}
 
- */
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
 
-//    hw 5 ex 17
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+/**Додай класу Admin наступні властивості і методи.
+Публічну властивість blacklistedEmails для зберігання чорного списку поштових адрес користувачів. Значення за замовчуванням — це порожній масив.
+
+Публічний метод blacklist(email) для додавання пошти у чорний список. Метод повинен додавати значення параметра email в масив, що зберігається у властивості blacklistedEmails.
+
+Публічний метод isBlacklisted(email) для перевірки пошти у чорному списку. Метод повинен перевіряти наявність значення параметра email в масиві, що зберігається у властивості blacklistedEmails, і повертати true або false.*/
+
+//    hw 5 ex 20
