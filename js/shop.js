79e284e5c861399daa09ea2a0f48ba99fa58eb62
ansachuk <ansachuk@icloud.com>
2023-03-07 22:36:09 +0200

    add makeMarkup function and render of markup

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,56 @@
+// import { iphonesList } from "./shop-products";
+
+const iphonesList = [
+  {
+    device: "Iphone",
+    model: "XR",
+    price: 220,
+    alt: "iphone",
+    src: "https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp",
+    id: "ph-1",
+  },
+  {
+    device: "Iphone",
+    model: "XR",
+    price: 221,
+    alt: "iphone",
+    src: "https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp",
+    id: "ph-1",
+  },
+  {
+    device: "Iphone",
+    model: "XR",
+    price: 222,
+    alt: "iphone",
+    src: "https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp",
+    id: "ph-1",
+  },
+  {
+    device: "Iphone",
+    model: "XR",
+    price: 223,
+    alt: "iphone",
+    src: "https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp",
+    id: "ph-1",
+  },
+  {
+    device: "Iphone",
+    model: "XR",
+    price: 220,
+    alt: "iphone",
+    src: "https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp",
+    id: "ph-1",
+  },
+  {
+    device: "Iphone",
+    model: "XR",
+    price: 220,
+    alt: "iphone",
+    src: "https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp",
+    id: "ph-1",
+  },
+];
+
 const navigationRef = document.querySelector("nav");
 const productsListRef = document.querySelector(".products__list");
 
@@ -8,25 +61,46 @@ function navClickHandler(e) {
     return;
   }
   const currentcategorie = e.target.dataset.categorie;
-  console.log(currentcategorie);
+  switch (currentcategorie) {
+    case "iphone":
+      makeCategoryMarkup(iphonesList);
+      break;
+
+    case "ipad":
+      makeCategoryMarkup();
+      break;
+
+    case "macbook":
+      makeCategoryMarkup();
+      break;
+
+    default:
+      break;
+  }
 }
 
-{
-  /* <li class="product__item">
+function makeCategoryMarkup(arr = null) {
+  const markup = arr
+    .map(
+      ({ src, alt, device, model, price, id }) => `<li class="product__item">
   <div class="product__item--wrapper">
     <img
-      src="https://jabko.ua/image/cache/catalog/products/2021/04/161625/black-420x420.jpg.webp"
-      alt="IPhone XR"
+      src="${src}"
+      alt="${alt}"
       width="100%"
       class="product__item--img"
     />
-    <h2 class="product__item--title">Iphone XR</h2>
-    <p class="product__item--price">1000$</p>
-    <button type="button" class="product__item--btn">
+    <h2 class="product__item--title">${device} ${model}</h2>
+    <p class="product__item--price">${price}$</p>
+    <button type="button" class="product__item--btn" data-id="${id}">
       <svg class="product__item--icon" width="40" height="40">
         <use href=""></use>
       </svg>
     </button>
   </div>
-</li>; */
+</li>`
+    )
+    .join("");
+
+  productsListRef.innerHTML = markup;
 }
